2b33f8495a288ec9f7a70dbc8c06fe17ecb798c1
Aniekan George Edet <aniekan.g.edet@gmail.com>
2025-04-14 21:03:15 +0100

    feat: add home and about page

---
+++ src/components/Side.tsx
@@ -0,0 +1,31 @@
+
+
+
+export default function Side() {
+  return (
+    <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+        <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+            <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+                About
+            </h2>
+        </div>
+        <div>
+            <p className="mb-4">
+                Professor Enoidem Usoro is an erudite scholar who has left an unerasable imprint in Business Education across the globe. She began her career as a teaching assistant in Unicross. In the University she has handled several key responsibilities. At present she is the Deputy Vice Chancellor (Academic) of Univeristy of Uyo.
+            </p>
+            <p className="mb-4">
+                Committees and Community Services within and outside University of Uyo have found her membership necessary due to her intelligible contributions and insights. Some of such committees includes Chairman, Committee on Affiliate Institutions with University of Uyo, Member, Committee On Quick Response to University of Uyo on Covid-19 and several others.
+            </p>
+            <p>
+                She holds Membership in several Professional Bobies such as National Association of Women in Academics (NAWACS), Association for the Advancement of Vocational Education of Nigeria where she serves as the National Treasurer.
+            </p>
+            <p>
+                Despite her busy schedule she has published several articles in National and International Journals, Book Chapters and Conference Papers. Many of which are open access on this knowledge base. In 2016, Professor Eno Usoro presented the 49th Inaugural Lecture of the University of Uyo on Business Education: Skills Acquisition and Development for Posterity.
+            </p>
+            <p>
+                She is married and blessed with 5 beautiful children.
+            </p>
+        </div>
+    </section>
+  );
+}
