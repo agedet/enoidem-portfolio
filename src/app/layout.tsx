a18550ce1abf5c1d7cf27e566438b2126db0df6a
Aniekan George Edet <aniekan.g.edet@gmail.com>
2025-04-19 21:46:50 +0100

    feat: add toast from sooner for notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
   variable: "--font-geist-sans",
@@ -41,10 +42,14 @@ export default function RootLayout({
       >
         <main className="min-h-screen mx-auto max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
           <a href="#content" className="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-primary via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0">Skip to Content</a>
+          
           <section className="lg:flex lg:justify-between lg:gap-4">
             <Header />
             
             <main id="content" className="pt-24 lg:w-[52%] lg:py-24">
+              <Toaster 
+                richColors position="top-right" 
+              />
               {children}
             </main>
           </section>
