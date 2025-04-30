974521a715ca17c834e978d95b0bc095044c0890
Aniekan George Edet <aniekan.g.edet@gmail.com>
2025-04-30 07:38:41 +0100

    fix: link behaviour

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs'
+import Link from 'next/link'
 
 const PUBLICATIONS_JOURNAL_ARTICLES = [
     {
@@ -155,21 +156,23 @@ function Publications() {
                     </ul>
 
                     <div className="mt-12">
-                        <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-primary focus-visible:text-primary font-semibold text-slate-200 group/link text-base" aria-label="View Full Project Archive" href="/archive">
-                            <span>
-                                <span className="border-b border-transparent pb-px transition group-hover:border-primary motion-reduce:transition-none">
-                                    View Full{' '} 
-                                </span>
-                                <span className="whitespace-nowrap">
+                        <Link href="/archive" aria-label="View Full Project Archive" legacyBehavior>
+                            <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-primary focus-visible:text-primary font-semibold text-slate-200 group/link text-base">
+                                <span>
                                     <span className="border-b border-transparent pb-px transition group-hover:border-primary motion-reduce:transition-none">
-                                        Publications
+                                        View Full{' '} 
                                     </span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" aria-hidden="true">
-                                        <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd"></path>
-                                    </svg>
+                                    <span className="whitespace-nowrap">
+                                        <span className="border-b border-transparent pb-px transition group-hover:border-primary motion-reduce:transition-none">
+                                            Publications
+                                        </span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" aria-hidden="true">
+                                            <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd"></path>
+                                        </svg>
+                                    </span>
                                 </span>
-                            </span>
-                        </a>
+                            </a>
+                        </Link>
                     </div>
                 </TabsContent>
 
@@ -233,7 +236,8 @@ function Publications() {
                     </ul>
 
                     <div className="mt-12">
-                        <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-primary focus-visible:text-primary font-semibold text-slate-200 group/link text-base" aria-label="View Full Project Archive" href="/archive">
+                        <Link href="/archive" aria-label="View Full Project Archive" legacyBehavior>
+                        <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-primary focus-visible:text-primary font-semibold text-slate-200 group/link text-base" >
                             <span>
                                 <span className="border-b border-transparent pb-px transition group-hover:border-primary motion-reduce:transition-none">
                                     View Full{' '} 
@@ -248,6 +252,7 @@ function Publications() {
                                 </span>
                             </span>
                         </a>
+                        </Link>
                     </div>
                 </TabsContent>
 
@@ -311,7 +316,8 @@ function Publications() {
                     </ul>
 
                     <div className="mt-12">
-                        <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-primary focus-visible:text-primary font-semibold text-slate-200 group/link text-base" aria-label="View Full Project Archive" href="/archive">
+                        <Link aria-label="View Full Project Archive" href="/archive" legacyBehavior>
+                        <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-primary focus-visible:text-primary font-semibold text-slate-200 group/link text-base" >
                             <span>
                                 <span className="border-b border-transparent pb-px transition group-hover:border-primary motion-reduce:transition-none">
                                     View Full{' '} 
@@ -326,6 +332,7 @@ function Publications() {
                                 </span>
                             </span>
                         </a>
+                        </Link>
                     </div>
                 </TabsContent>
             </Tabs>
