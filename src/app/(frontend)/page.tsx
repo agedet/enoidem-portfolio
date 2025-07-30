7ce119797241641bf12fd99843ad2f9a7186a17f
Aniekan George Edet <aniekan.g.edet@gmail.com>
2025-07-30 17:05:16 +0100

    feat: add Navbar

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -7,24 +7,29 @@ import Stats from "@/components/Stats";
 import Header from "@/components/Header";
 import Associations from "@/components/Associations";
 import ASFooter from "@/components/ASFooter";
-import MobileHeader from "@/components/MobileHeader";
+import NavBar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background leading-relaxed text-text antialiased selection:bg-primary selection:text-text expansion-alids-init">
        <section>
-          <MobileHeader />
+          <NavBar />
         </section>
       <main className="min-h-screen mx-auto max-w-screen-xl px-4 py-12 font-sans md:px-12 md:py-16 lg:py-0">
         <a href="#content" className="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-primary via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0">Skip to Content</a>
 
-       
-          
-        <section className="lg:flex lg:justify-between lg:gap-4">
+        <section className="lg:flex lg:justify-between lg:gap-4 pt-24 lg:py-24">
+          <main id="content">
+            <Hero />
+          </main>
+        </section>
+
+        {/* <section className="lg:flex lg:justify-between lg:gap-4">
           <Header />
-          
           <main id="content" className="pt-24 lg:w-[52%] lg:py-24">
+            <Hero />
             <About />
             <Stats />
             <Education />
@@ -34,7 +39,8 @@ export default function Home() {
             <Contact />
             <ASFooter />
           </main>
-        </section>
+        </section> */}
+
       </main>
     </div>
   );
